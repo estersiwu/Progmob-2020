3f5726692a59ea07e3201226142b1040a577a09e
estersiwu <ester.glorya@si.ukdw.ac.id>
2020-10-02 20:41:16 +0700

    tugas_2

--- app/src/main/java/com/example/progmob_2020/MainActivity.java
+++ app/src/main/java/com/example/progmob_2020/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,13 +14,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.tes_constraint_layout);
+        setContentView(R.layout.activity_main);
+
 
         //variable
-        /*final TextView txtView = (TextView)findViewById(R.id.mainActivityTextView);
-        Button myBtn = (Button)findViewById(R.id.button1);
-        final EditText myEditText = (EditText)findViewById(R.id.editText1);
-        Button btnHelp = (Button)findViewById(R.id.btnHelp);
+        final TextView txtView = (TextView) findViewById(R.id.mainActivityTextView);
+        Button myBtn = (Button) findViewById(R.id.button1);
+        final EditText myEditText = (EditText) findViewById(R.id.editText1);
+        Button btnHelp = (Button) findViewById(R.id.btnHelp);
+        final  TextView myView = (TextView) findViewById(R.id.mainActivityTextView);
+        Button btnTracker = (Button) findViewById(R.id.btnTracker);
 
         //action
         txtView.setText(R.string.text_hello_world);
@@ -44,6 +46,14 @@ public class MainActivity extends AppCompatActivity {
 
                 startActivity(intent);
             }
-        }};*/
+        });
+
+        btnTracker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TrackerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/progmob_2020/TrackerActivity.java
@@ -0,0 +1,16 @@
+package com.example.progmob_2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class TrackerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tracker);
+
+    }
+}
