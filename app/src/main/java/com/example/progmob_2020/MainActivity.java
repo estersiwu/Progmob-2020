b11e7321ad7899449de81c3748befb1a8c0ab1e3
estersiwu <ester.glorya@si.ukdw.ac.id>
2020-10-02 08:55:16 +0700

    tugas_2_tutorial

--- app/src/main/java/com/example/progmob_2020/MainActivity.java
+++ app/src/main/java/com/example/progmob_2020/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.progmob_2020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //variable
+        final TextView txtView = (TextView)findViewById(R.id.mainActivityTextView);
+        Button myBtn = (Button)findViewById(R.id.button1);
+        final EditText myEditText = (EditText)findViewById(R.id.editText1);
+
+        //action
+        txtView.setText(R.string.text_hello_world);
+        myBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Log.d("Coba Klik..",myEditText.getText().toString());
+                txtView.setText(myEditText.getText().toString());
+            }
+        });
     }
 }
